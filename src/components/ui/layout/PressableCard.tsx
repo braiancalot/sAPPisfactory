a5b0999179dc7bd3daa18829b09e183c2c414b07
braiancalot <calot665@hotmail.com>
2025-11-06 07:32:24 -0300

    feat: add @ui/layout/Card and @ui/layout/PressableCard

---
+++ src/components/ui/layout/PressableCard.tsx
@@ -0,0 +1,24 @@
+import { Pressable } from "react-native";
+
+type Props = {
+  children: React.ReactNode;
+  onPress: () => void;
+  onLongPress: () => void;
+};
+
+export default function PressableCard({
+  children,
+  onPress,
+  onLongPress,
+}: Props) {
+  return (
+    <Pressable
+      className="bg-surface-2 active:bg-surface-4 p-lg rounded-lg"
+      style={{ elevation: 2 }}
+      onPress={onPress}
+      onLongPress={onLongPress}
+    >
+      {children}
+    </Pressable>
+  );
+}
