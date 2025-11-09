e3772d37d867503b8c238d7197a3c1995995d8cf
braiancalot <calot665@hotmail.com>
2025-11-09 09:58:47 -0300

    feat: add @ui/ScreenContainer and @ui/ScrollScreenContainer

---
+++ src/components/ui/ScreenContainer.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ScreenContainer({ children }: Props) {
+  return <View className="flex-1 bg-background p-xl">{children}</View>;
+}
