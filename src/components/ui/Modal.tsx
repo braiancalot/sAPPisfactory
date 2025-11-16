eebf55353e6cdb830a26b7c68e3ecd3a4d31d2e3
braiancalot <calot665@hotmail.com>
2025-11-16 18:54:51 -0300

    refactor: using reanimated in Modal and make compatible with BottomSheet

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,53 +1,114 @@
-import { View, Modal as RNModal, Pressable } from "react-native";
+import { View, Pressable, Modal as RNModal } from "react-native";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@theme/colors";
 
 import Text from "@ui/Text";
+import { useEffect, useState } from "react";
 
 type Props = {
   visible: boolean;
   onClose: () => void;
   title: string;
-  animationType?: "fade" | "slide" | "none";
   children: React.ReactNode;
 };
 
-export default function Modal({
-  visible,
-  onClose,
-  title,
-  animationType = "slide",
-  children,
-}: Props) {
+export default function Modal({ visible, onClose, title, children }: Props) {
+  const [isRendered, setIsRendered] = useState(false);
+
+  const backdropOpacity = useSharedValue(0);
+  const modalScale = useSharedValue(0.9);
+  const modalOpacity = useSharedValue(0);
+
+  useEffect(() => {
+    if (visible) {
+      setIsRendered(true);
+
+      backdropOpacity.value = withTiming(1, { duration: 200 });
+      modalOpacity.value = withTiming(1, { duration: 150 });
+      modalScale.value = withSpring(1, {
+        damping: 15,
+        stiffness: 150,
+        mass: 0.3,
+      });
+    } else {
+      backdropOpacity.value = withTiming(0, { duration: 150 });
+      modalScale.value = withTiming(0.9, { duration: 150 });
+      modalOpacity.value = withTiming(0, { duration: 150 }, (finished) => {
+        if (finished) {
+          runOnJS(setIsRendered)(false);
+        }
+      });
+    }
+  }, [visible]);
+
+  const backdropStyle = useAnimatedStyle(() => ({
+    opacity: backdropOpacity.value,
+  }));
+
+  const modalStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: modalScale.value }],
+    opacity: modalOpacity.value,
+  }));
+
+  if (!isRendered) return null;
+
   return (
     <RNModal
-      visible={visible}
-      transparent={true}
-      animationType={animationType}
+      visible={isRendered}
       onRequestClose={onClose}
+      transparent
+      statusBarTranslucent
+      animationType="none"
     >
-      <View className="flex-1 items-center justify-center bg-overlay">
-        <Pressable
-          onPress={onClose}
-          className="absolute top-0 left-0 right-0 bottom-0"
-        />
-        <View className="bg-surface-2 p-lg rounded-lg w-[90%] max-w-[500]">
-          <View className="flex-row justify-between items-center mb-xl">
-            <Text variant="title" className="text-text-primary">
-              {title}
-            </Text>
-
-            <Pressable onPress={onClose} className="active:scale-95">
-              <MaterialIcons
-                name="close"
-                size={24}
-                color={colors["text-secondary"]}
+      <GestureHandlerRootView>
+        <BottomSheetModalProvider>
+          <View className="flex-1 items-center justify-center">
+            <Animated.View
+              className="bg-overlay absolute top-0 left-0 right-0 bottom-0"
+              style={backdropStyle}
+            >
+              <Pressable
+                onPress={onClose}
+                className="absolute top-0 left-0 right-0 bottom-0"
               />
-            </Pressable>
+            </Animated.View>
+
+            <Animated.View
+              className="w-full items-center justify-center"
+              style={modalStyle}
+            >
+              <View className="bg-surface-2  rounded-lg w-[90%] max-w-[500]">
+                <View className="flex-row justify-between items-center px-lg pt-lg pb-md border-b border-border">
+                  <Text variant="title" className="text-text-primary">
+                    {title}
+                  </Text>
+
+                  <Pressable onPress={onClose} className="active:scale-95">
+                    <MaterialIcons
+                      name="close"
+                      size={24}
+                      color={colors["text-secondary"]}
+                    />
+                  </Pressable>
+                </View>
+
+                <View className="px-lg py-lg">{children}</View>
+              </View>
+            </Animated.View>
           </View>
-          {children}
-        </View>
-      </View>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </RNModal>
   );
 }
